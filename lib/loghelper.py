427c075e4bacfe54a46d31f4f9fe67c58d8f1a56
Troy Honegger <troyhonegger@gmail.com>
2019-05-05 22:39:49 -0400

    format the logs properly, and auto-rotate them once a day

--- lib/loghelper.py
+++ lib/loghelper.py
@@ -1,12 +1,15 @@
 # For logging API documentation see https://docs.python.org/3/library/logging.html
 
 import logging
+from logging import handlers
 import os
 
 def get_logger(file):
 	logger = logging.getLogger('agbot')
 	if not logger.hasHandlers():
-		logger.addHandler(logging.FileHandler('/var/data/logs/agbot.log'))
+		handler = handlers.TimedRotatingFileHandler('/var/data/logs/agbot.log', when = 'midnight', backupCount = 31)
+		handler.setFormatter(logging.Formatter('%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s'))
+		logger.addHandler(handler)
 	if file is not None:
 		child = logging.getLogger('agbot.%s'%(os.path.splitext(file)[0]))
 		child.setEffectiveLevel(logging.DEBUG)
