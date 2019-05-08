ff8a14a63216d946cd637ddf36a9b0952f9dfb5e
Troy Honegger <troyhonegger@gmail.com>
2019-05-08 16:54:24 -0400

    made scripts with 'main' method executable; add a couple logging fixes

--- lib/loghelper.py
+++ lib/loghelper.py
@@ -7,10 +7,10 @@ import os
 def get_logger(file):
 	logger = logging.getLogger('agbot')
 	if not logger.hasHandlers():
-		handler = handlers.TimedRotatingFileHandler('/var/data/logs/agbot.log', when = 'midnight', backupCount = 31)
+		handler = handlers.TimedRotatingFileHandler('/var/log/agbot.log', when = 'midnight', backupCount = 31)
 		handler.setFormatter(logging.Formatter('%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s'))
 		logger.addHandler(handler)
 	if file is not None:
 		child = logging.getLogger('agbot.%s'%(os.path.splitext(file)[0]))
-		child.setEffectiveLevel(logging.DEBUG)
+		child.setLevel(logging.DEBUG)
 		return child
