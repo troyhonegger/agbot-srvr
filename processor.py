66a2b19524ab98fd533dfdecf523dbbbe231cf50
Troy Honegger <troyhonegger@gmail.com>
2019-05-09 19:12:23 -0400

    added a diag mode to the processor, to (1) allow unfinished modules to be turned off, and (2) display an optional live output of one camera

--- processor.py
+++ processor.py
@@ -5,6 +5,7 @@ import datetime
 import shutil
 import signal
 import setproctitle
+import cv2
 
 from lib import records
 from lib import cameras
@@ -47,15 +48,32 @@ def map_location(camera_id, x, y):
 	else:
 		return None
 
+def _draw_bbox(img, cls, x, y, z, w, h):
+	if cls == 'foxtail':
+		color = (0, 0, 255) #red
+	elif cls == 'corn':
+		color = (0, 255, 0) #green
+	elif cls == 'nitro_def_corn':
+		color = (0, 255, 246) #yellow
+	elif cls == 'cocklebur':
+		color = (255, 0, 250) #pink
+	elif cls == 'giant_ragweed':
+		color = (255, 0, 0) #blue
+	else:
+		color = (0, 0, 0) #black
+	pt1 = ((x-w/2) * len(img[0]), (y-h/2) * len(img))
+	pt2 = ((x+w/2) * len(img[0]), (y+h/2) * len(img))
+	cv2.rectangle(img, pt1, pt2, color)
+
 plants_map = {
-        'foxtail': plants.Plants.Foxtail,
-        'cocklebur': plants.Plants.Cocklebur,
+    'foxtail': plants.Plants.Foxtail,
+    'cocklebur': plants.Plants.Cocklebur,
 	'giant_ragweed': plants.Plants.Ragweed,
-        'nitro_def_corn': plants.Plants.Corn,
-        'corn': plants.Plants.NONE # ignore non-nitrogen deficient corn
+    'nitro_def_corn': plants.Plants.Corn,
+    'corn': plants.Plants.NONE # ignore non-nitrogen deficient corn
 }
 
-def start_processor():
+def start_processor(ignore_multivator = False, ignore_speed_controller = False):
 	global net
 	global meta
 	global cams
@@ -76,14 +94,17 @@ def start_processor():
 	cams = cameras.open_cameras('id*')
 	cams_history= [None for cam in cams]
 	log.debug('Opened cameras - %d found', len(cams))
-	mult = multivator.Multivator(initial_mode = multivator.Mode.processing)
-	mult.connect()
-	speed_controller = speed_ctrl.SpeedController()
-	speed_controller.connect()
-	speed_controller.start()
-	log.debug('Connected to multivator and speed controller.')
+	if not ignore_multivator:
+		mult = multivator.Multivator(initial_mode = multivator.Mode.processing)
+		mult.connect()
+		log.debug('Connected to multivator')
+	if not ignore_speed_controller:
+		speed_controller = speed_ctrl.SpeedController()
+		speed_controller.connect()
+		speed_controller.start()
+		log.debug('Connected to speed controller')
 
-def process_detector():
+def process_detector(ignore_multivator = False, ignore_nmea = False, diagcam_id = None):
 	global net
 	global meta
 	global cams
@@ -94,6 +115,7 @@ def process_detector():
 	i = -1
 	results = [plants.Plants.NONE] * 5
 	for camera in cams:
+		draw_bbox = camera.id == diagcam_id
 		i += 1 # increment i here just so we don't forget if we add a continue or something to the loop
 		ret, image = camera.read()
 		if not ret: #ERROR - skip this camera
@@ -104,17 +126,22 @@ def process_detector():
 		else:
 			cams_history[i] = True
 		for (cls, confidence, (x, y, w, h)) in darknet_wrapper.detect_cv2(net, meta, image, thresh = THRESHOLD):
+			if draw_bbox:
+				draw_bbox(image, cls, x, y, w, h)
+				cv2.imshow(diagcam_id, image)
 			if cls in plants_map.keys():
 				results[map_location(camera.id, x, y)] |= plants_map[cls]
-	mult.send_process_message(plants)
-	gga = nmea.read_data(nmea.GGA)
-	record = records.RecordLine(datetime.datetime.now(), gga.longitude, gga.latitude, results)
-	print(str(record), file)
-	file.flush()
+	if not ignore_multivator:
+		mult.send_process_message(results)
+	if not ignore_nmea:
+		gga = nmea.read_data(nmea.GGA)
+		record = records.RecordLine(datetime.datetime.now(), gga.longitude, gga.latitude, results)
+		print(str(record), file)
+		file.flush()
 
-def process():
+def process(ignore_multivator = False, ignore_speed_controller = False, ignore_nmea = False, diagcam_id = None):
 	# TODO: update this with end-of-row detection and handling
-	process_detector()
+	process_detector(ignore_multivator, ignore_nmea, diagcam_id)
 
 def stop_processor():
 	log.info('Shutting down processor...')
@@ -171,14 +198,12 @@ def sigint_handler(sig, frame):
 	sigint_received = True
 	signal.signal(signal.SIGINT, sigint_handler)
 
-def main():
-	start_processor()
+def main(ignore_multivator = False, ignore_speed_controller = False, ignore_nmea = False, diagcam_id = None):
+	start_processor(ignore_multivator, ignore_speed_controller)
 	try:
-		while True:
-			process()
-			if sigint_received:
-				log.info('Received SIGINT - terminating processor.')
-				break
+		while not sigint_received:
+			process(ignore_multivator, ignore_speed_controller, ignore_nmea, diagcam_id)
+		log.info('Received SIGINT - terminating processor.')
 	except Exception as exception:
 		log.exception(exception)
 		raise
@@ -186,10 +211,18 @@ def main():
 		stop_processor()
 
 if __name__ == '__main__':
+	import argparse
 	# Set the process title so we can be found (and signaled) more easily
 	os.environ['SPT_NOENV'] = 'True'
 	setproctitle.setproctitle('processor.py')
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-c', '--diagcam-id', default = None)
+	parser.add_argument('-n', '--ignore-nmea', action = 'store_true')
+	parser.add_argument('-m', '--ignore-multivator', action = 'store_true')
+	parser.add_argument('-s', '--ignore-speed-controller', action = 'store_true')
+	args = parser.parse_args()
 	# register SIGINT handler
 	signal.signal(signal.SIGINT, sigint_handler)
-	main()
+	main(args.ignore_multivator, args.ignore_speed_controller, args.ignore_nmea, args.diagcam_id)
 
