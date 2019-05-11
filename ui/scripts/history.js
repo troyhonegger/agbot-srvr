97706ff279759eae1456bb515aa49f2e6a6cbd22
Troy Honegger <troyhonegger@gmail.com>
2019-05-11 10:34:22 -0400

    display all previous records (front-end)

---
+++ ui/scripts/history.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+    $.ajax({
+        url: '../api/records',
+        type: 'GET',
+        success: function (msg) {
+            if (msg.length !== 0) {
+                var historyTable = $('#history_table');
+                for (var i = 0; i < msg.length; i++) {
+                    historyTable.append('<tr><td><a href="../api/records/'+msg[i].recordID+'">'+msg[i].name+'</a></td></tr>');
+                }
+                historyTable.css('visibility', 'visible');
+                $('#history_notfound').css('visibility', 'collapse');
+            }
+        },
+        error: function (msg) {
+            // log the error message, but don't do anything else; just leave the default "no records found" content
+            console.log(msg);
+        }
+    });
+});
